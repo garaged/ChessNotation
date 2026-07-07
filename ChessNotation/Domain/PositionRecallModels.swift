402935eca2ea754de5cc571dbf2879d6af3155f6
garaged <garaged@gmail.com>
2026-07-07 17:12:28 -0600

    Implement notation concept game foundation (#7)
    * Add SAN builder component model
    
    * Implement SAN builder parser
    
    * Add position recall prompt model
    
    * Add notation concept game tests

---
+++ ChessNotation/Domain/PositionRecallModels.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+enum PositionRecallQuestion: String, Codable {
+    case locatePiece
+    case squareOccupant
+    case occupiedSubset
+}
+
+struct PositionRecallItem: Hashable, Codable {
+    let name: String
+    let square: ChessSquare
+}
+
+struct PositionRecallPrompt: Hashable, Codable {
+    let items: [PositionRecallItem]
+    let question: PositionRecallQuestion
+    let requestedName: String?
+    let requestedSquare: ChessSquare?
+    let subset: Set<ChessSquare>
+    let orientation: BoardOrientationPolicy
+    let studyDuration: TimeInterval
+
+    var expectedSquares: Set<ChessSquare> {
+        switch question {
+        case .locatePiece:
+            guard let requestedName else { return [] }
+            guard let item = items.first(where: { $0.name == requestedName }) else { return [] }
+            return [item.square]
+        case .squareOccupant:
+            guard let requestedSquare else { return [] }
+            return items.contains(where: { $0.square == requestedSquare }) ? [requestedSquare] : []
+        case .occupiedSubset:
+            return Set(items.map { $0.square }).intersection(subset)
+        }
+    }
+}
